dd3ca1233bf0de4b1a39749372988ccd57282266
Zaid Aslam <122831945+zaidaslam99@users.noreply.github.com>
2024-01-23 12:28:06 -0600

    update

--- main2.py
+++ main2.py
@@ -1,8 +1,16 @@
 from bs4 import BeautifulSoup
 import requests
 
+# Here we are getting the html text from the website
+html_text = requests.get("https://www.timesjobs.com/candidate/job-search.html?searchType=personalizedSearch&from=submit&searchTextSrc=&searchTextText=&txtKeywords=python&txtLocation=").text
 
-html_text = requests.get("https://www.timesjobs.com/candidate/job-search.html?searchType=Home_Search&from=submit&asKey=OFF&txtKeywords=&cboPresFuncArea=28").text
+# Here we are going to be using an lxml parser
 soup = BeautifulSoup(html_text, "lxml")
-jobs = soup.find_all("li", class_ = "clearfix job-bx wht-shd-bx")
-print(jobs)
+
+# Brings results for the first page 
+jobs = soup.find("li", class_ ='clearfix job-bx wht-shd-bx')
+
+company_name = jobs.find("h3", class_ = "joblist-comp-name").text.replace(" ", "")
+skills = jobs.find("span", class_ = "srp-skills")
+
+print(skills)
